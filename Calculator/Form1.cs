8a4ee876602c90ea870d917bc1945ee14b20d172
Nour <nowraldo@gmail.com>
2024-04-07 06:22:41 +0200

    make delete button

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -1,10 +1,13 @@
+using System;
+
 namespace Calculator
 {
     public partial class Form1 : Form
     {
         double Result;
         string Operator = "";
-        bool isOPeraten = false;
+        bool isOperator;
+
         public Form1()
         {
             InitializeComponent();
@@ -13,22 +16,27 @@ namespace Calculator
         private void EnterNumbers(object sender, EventArgs e)
         {
             Button num = (Button)sender;
-            if (Output.Text == "0" || isOPeraten)
-                Output.Text = "";
+            if (Output.Text == "0" || isOperator)
             {
-                if (num.Text == ".")
+                Output.Text = "";
+                OutputTracker.Text += Output.Text;
+            }
+
+            if (num.Text == ".")
+            {
+                if (!Output.Text.Contains("."))
                 {
-                    if (!Output.Text.Contains("."))
-                    {
-                        Output.Text = Output.Text + num.Text;
-                    }
-                }
-                else
-                {
-                    Output.Text = Output.Text + num.Text;
-                    isOPeraten = false;
+                    Output.Text += num.Text;
+                    OutputTracker.Text += Output.Text;
                 }
             }
+            else
+            {
+                Output.Text += num.Text;
+                OutputTracker.Text += num.Text;
+                isOperator = false;
+            }
+
         }
 
         private void operations(object sender, EventArgs e)
@@ -36,11 +44,13 @@ namespace Calculator
             Button num = (Button)sender;
             Result = Convert.ToDouble(Output.Text);
             Operator = num.Text;
-            isOPeraten = true;
+            OutputTracker.Text += num.Text;
+            isOperator = true;
         }
 
         private void BtnEqual_Click(object sender, EventArgs e)
         {
+
             switch (Operator)
             {
                 case "+":
@@ -61,6 +71,7 @@ namespace Calculator
                 default:
                     break;
             }
+            OutputTracker.Text = "";
 
         }
 
@@ -68,6 +79,29 @@ namespace Calculator
         {
             Output.Text = "0";
             Result = 0;
+            OutputTracker.Text = "";
+        }
+
+        private void Delete_Character_Click(object sender, EventArgs e)
+        {
+            if ((Button)sender is not null)
+            {
+                Output.Text = OutputTracker.Text = removeFinalChar(Output.Text);
+
+
+
+            }
+        }
+        private string removeFinalChar(string text)
+        {
+            if (text != string.Empty)
+            {               
+                return text.Substring(0, text.Length - 1);
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
     }
 }
